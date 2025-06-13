24ae0992c015895f4d8917025c8de229ea9368e2
coelitherese <ctg-brillante.student@ua.edu.ph>
2025-06-13 09:25:57 +0800

    Session 2 Acivity Added

---
+++ session2/activity.js
@@ -0,0 +1,31 @@
+console.log("Item 01.")
+function addNum(addNum1 , addNum2){
+    sum = addNum1 + addNum2
+    console.log("The sum of " + addNum1 + " and " + addNum2 + " is: ")
+    console.log(sum);
+}
+function subNum(subNum1 , subNum2){
+    difference = subNum1 - subNum2
+    console.log("The difference of " + subNum1 + " and " + subNum2 + " is: ")
+    console.log(difference);
+}
+
+console.log("Item 02.")
+function multiplyNum(multiplyNum1 , multiplyNum2){
+    product = multiplyNum1 * multiplyNum2
+    console.log("The product of " + multiplyNum1 + " and " + multiplyNum2 + " is: ")
+    console.log(product);
+}
+function divideNum(divideNum1 , divideNum2){
+    quotient = divideNum1 / divideNum2
+    console.log("The quotient of " + divideNum1 + " and " + divideNum2 + " is: ")
+    console.log(quotient);
+}
+
+console.log("Item 03.")
+function getCircleArea(radius1){
+    const pi = 3.14
+    circleArea = pi * radius1 ** 2
+    console.log("FORMULA USED: A=πr2");
+    console.log("The area of the circle is: " + circleArea);
+}
